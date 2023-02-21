e73a894a063a50e140a0fc6a6a3b8ff1553c12e5
Luís Henrique <lhmaruyama@gmail.com>
2023-02-20 21:52:55 -0300

    fix: database

--- src/Home.js
+++ src/Home.js
@@ -1,14 +1,19 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import { signOut } from "firebase/auth"
 import { auth, storage, db } from "./firebase.js"
 import { getStorage, ref, uploadBytes, getDownloadURL, uploadBytesResumable } from "firebase/storage"
-import { addDoc, collection, doc, setDoc } from "firebase/firestore"
+import { addDoc, collection, doc, setDoc, onSnapshot, query, where } from "firebase/firestore"
 import "./Home.css"
 import { GoPlus } from "react-icons/go"
-import { ImGoogleDrive } from "react-icons/im"
+
+import { GrCheckboxSelected, GrCheckbox } from "react-icons/gr"
+import { AiOutlineFileExcel, AiOutlineFileWord, AiOutlineFileImage, AiOutlineFilePdf, AiOutlineFileText, AiOutlineFile, AiOutlineFileZip } from "react-icons/ai"
+import { BsFileEarmarkExcel, BsFileEarmarkWord, BsFileEarmarkImage, BsFileEarmarkPdf, BsFileEarmarkFont, BsFileEarmark, BsFileEarmarkZip, BsFileEarmarkMusic, BsFileEarmarkPlay } from "react-icons/bs"
+
+
 import { RiArchiveDrawerLine, RiComputerLine } from "react-icons/ri"
 import { FiUsers } from "react-icons/fi"
-import { BsClock } from "react-icons/bs"
+import { BsClock, BsCloudArrowDown } from "react-icons/bs"
 import { BiTrash } from "react-icons/bi"
 import Logo from "./logo-drive-ok.png"
 import Searchs from "../src/icon/searchs.png"
@@ -35,22 +40,55 @@ function Home(props) {
     }
 
     const [progress, setProgress] = useState(0)
+    const [archive, setArchive] = useState([])
+    const [iconSelect, setIconSelect] = useState(1)
+
+    function changeButton(){
+        (iconSelect==0)?setIconSelect(1):setIconSelect(0)
+    }
+    useEffect(() =>{
+    }, [])
+
+    useEffect(() => {
+        //const myref = doc(collection(db, "drive", props.credential.uid, "files"))
+        //const myref = doc(collection(db, "drive"))
+        //const myref = query(collection(db, "drive"))
+        const myref = query(collection(db, "drive", props.credential.uid, "files"))
+        onSnapshot(myref, (snapshot) => {
+
+            const setArch = snapshot.docs.map((list) => {
+                //console.log(list.data())
+                return list.data()
+            })
+            //setArchive()
+            setArchive(setArch)
+        })
+
+    }, [])
+
 
     async function uploadFile(uid) {
         let file = document.querySelector("#new-file").files[0]
         let refFile = ref(storage, "myfiles/" + file.name)
         //let file = document.querySelector("[name=file]").files[0]
-        //console.log(file.name)
+        //const today = new Date()
+        const options = { day: "numeric", month: "short", year: "numeric" }
+        //const today = file.lastModifiedDate.getDate() + "/" + file.lastModifiedDate.getMonth() + "/" + file.lastModifiedDate.getFullYear()
+        const today = file.lastModifiedDate
+        const date = today.toLocaleDateString("pt-BR", options)
+        const size = Math.ceil(file.size / 1024)
+        //console.log(date)
+        //console.log(file.size)
 
         const uploadTask = uploadBytesResumable(refFile, file)
         uploadTask.on("state_changed", (snapshot) => {
             const change = (snapshot.bytesTransferred / snapshot.totalBytes)
             setProgress(change)
-            console.log("progress: " + change*100 + "%")
+            //console.log("progress: " + change * 100 + "%")
         })
 
         await uploadBytes(refFile, file)
-        .then(() => {
+            .then(() => {
 
                 //console.log("Successfully uploaded to storage")
                 //setProgress(0)
@@ -60,18 +98,18 @@ function Home(props) {
         //const storRef = ref(stor, "myfiles/" + file.name)
         //console.log(storRef === refFile)
         //false   
-        
-        await getDownloadURL(refFile)
-        .then((url) => {
-            //const myref = doc(db,"files/subfiles/item/subitem") subcoleção com ID personalizado
-            //const myref = doc(db,"files", "subfiles", "item", "subitem")
-            const myref = doc(collection(db, "drive", uid, "files"))
-            setDoc(myref, { fileUrl: url, type: file.type })
 
-            //console.log("Successfully saved data in firestore")
-            setProgress(0)
-            console.log("progress: " + progress + "%")
-        }).catch(err => { console.log(err) })
+        await getDownloadURL(refFile)
+            .then((url) => {
+                //const myref = doc(db,"files/subfiles/item/subitem") subcoleção com ID personalizado
+                //const myref = doc(db,"files", "subfiles", "item", "subitem")
+                const myref = doc(collection(db, "drive", uid, "files"))
+                setDoc(myref, { fileUrl: url, type: file.type, name: file.name, size: size, date: date })
+
+                //console.log("Successfully saved data in firestore")
+                setProgress(0)
+                //console.log("progress: " + progress + "%")
+            }).catch(err => { console.log(err) })
 
     }
 
@@ -93,12 +131,14 @@ function Home(props) {
                     <img className="photo" src={props.credential.image} />
                 </div>
             </div>
+
             <div className="sidebar-container" >
                 <div className="sidebar" >
                     <form className="sidebar-form" >
                         <label htmlFor="new-file" className="sidebar-button"><GoPlus />Novo</label>
                         <input onChange={() => uploadFile(props.credential.uid)} id="new-file" className="hidden-input" type="file" name="file" />
                     </form>
+
                     <div className="folders" >
                         <div className="my-folder" >
                             <RiArchiveDrawerLine /><span>Meu Drive</span>
@@ -118,11 +158,36 @@ function Home(props) {
                     </div>
                 </div>
 
-
                 <div className="content" >
                     <div className="top-content" >
                         <span>Meu Drive</span>
                     </div>
+                    <div className="header-content" >
+                        <h5 className="header-name"><GrCheckbox onClick={() => { alert("hiuiuiu") }} />Nome</h5>
+                        <h5 className="header-owner">Propietário</h5>
+                        <h5 className="header-date">Data</h5>
+                        <h5 className="header-size">Tamanho do arquivo</h5>
+                    </div>
+
+                    <div className="boxFiles">
+                        {
+                            archive.map(data => {
+                                // AiOutlineFileExcel, AiOutlineFileWord, AiOutlineFileImage, AiOutlineFilePdf, AiOutlineFileText, AiOutlineFile, AiOutlineFileZip
+                                //console.log(data.type.split("/")[1])
+                                //const image1 = <GrCheckbox onClick={()=>{alert("hiuiuiu")}}/>
+                                const image2 = <BsCloudArrowDown className="iconDown" />
+                                return (
+                                    <div className="boxFile">
+                                        <span onClick={()=>changeButton()} className="header-name-field">{iconSelect}{image2} {data.name}</span>
+                                        <span className="header-owner-field">{props.credential.name}</span>
+                                        <span className="header-date-field">{data.date}</span>
+                                        <span className="header-size-field">{data.size + " KB"}</span>
+                                    </div>
+                                )
+                            })
+                        }
+                    </div>
+
                     <div >
                         {
                             (progress > 0) ?
@@ -139,9 +204,12 @@ function Home(props) {
 
 
 
-            <h2>Logado</h2>
+            <h2>{new Date().getDate()}</h2>
             <h2>{props.credential.name}</h2>
             <a onClick={(e) => loggout(e)} href='#'>Sair</a>
+            <div>
+
+            </div>
 
 
             {/*             <img alt="" className="logo" src={Logo}  />
